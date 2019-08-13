6298c3e5dabb0ab1c9db67809050645a9e09f5a7
BuldakovIvan <iv081198@yandex.ru>
2019-08-13 12:37:13 +0300

    fix swiper and background-shadow integration

--- src/js/main.js
+++ src/js/main.js
@@ -6,21 +6,29 @@ let backgroundShadow = document.querySelector(".background-shadow");
 let slideButtonLeft = document.querySelector(".main-slide__button");
 let slideButtonRight = document.querySelector(".main-slide__button.main-slide__button_right");
 
-slideButtonLeft.addEventListener("mouseover", () => {
-	backgroundShadow.style.transform = 'translateX(-50%)';
-});
+function showBackgroundLeft() {
+    backgroundShadow.style.transform = 'translateX(-50%)';
+}
 
-slideButtonLeft.addEventListener("mouseout", () => {
-	backgroundShadow.style.transform = 'translateX(-100%)';
-});
+function showBackgroundRight() {
+    backgroundShadow.style.transform = 'translateX(50%)';
+}
 
-slideButtonRight.addEventListener("mouseover", () => {
-	backgroundShadow.style.transform = 'translateX(50%)';
-});
+function hideBackgroundLeft() {
+    backgroundShadow.style.transform = 'translateX(-100%)';
+}
 
-slideButtonRight.addEventListener("mouseout", () => {
-	backgroundShadow.style.transform = 'translateX(100%)';
-});
+function hideBackgroundRight() {
+    backgroundShadow.style.transform = 'translateX(100%)';
+}
+function addButtonsListeners() {
+    slideButtonLeft.addEventListener("mouseover", showBackgroundLeft);
+    slideButtonLeft.addEventListener("mouseout", hideBackgroundLeft);
+    slideButtonRight.addEventListener("mouseover", showBackgroundRight);
+    slideButtonRight.addEventListener("mouseout", hideBackgroundRight);
+}
+
+addButtonsListeners();
 
 let mainSwiper = new Swiper(".main-slider", {
     wrapperClass: "main-slider__wrapper",
@@ -28,10 +36,20 @@ let mainSwiper = new Swiper(".main-slider", {
     direction: "vertical",
     slidesPerView: 1,
     mousewheel: true,
+    on: {
+        reachBeginning: () => {
+            backgroundShadow.style.transform = 'translateX(-100%)';
+        },
+        slideNextTransitionStart: () => {
+            backgroundShadow.style.transform = 'translateX(0%)';
+            slideButtonLeft.removeEventListener("mouseout", hideBackgroundLeft);
+            slideButtonRight.removeEventListener("mouseout", hideBackgroundRight);
+        },
+    },
 });
 
 /*
- 
+
 import yuor vue modules example
 
 import Module from '../widgets/module';
