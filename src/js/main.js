aa07e2257629392b3d310db3daf8f0a3c1419075
GanterPenguin <iv081198@yandex.ru>
2019-08-13 03:22:23 +0300

    add background moving on button hover

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,24 @@
 'use strict';
 
+let backgroundShadow = document.querySelector(".background-shadow");
+let slideButtonLeft = document.querySelector(".main-slide__button");
+let slideButtonRight = document.querySelector(".main-slide__button.main-slide__button_right");
 
+slideButtonLeft.addEventListener("mouseover", () => {
+	backgroundShadow.style.transform = 'translateX(-50%)';
+});
+
+slideButtonLeft.addEventListener("mouseout", () => {
+	backgroundShadow.style.transform = 'translateX(-100%)';
+});
+
+slideButtonRight.addEventListener("mouseover", () => {
+	backgroundShadow.style.transform = 'translateX(50%)';
+});
+
+slideButtonRight.addEventListener("mouseout", () => {
+	backgroundShadow.style.transform = 'translateX(100%)';
+});
 /*
  
 import yuor vue modules example
