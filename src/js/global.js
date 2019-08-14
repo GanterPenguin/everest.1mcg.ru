3bb6a257f387568461cc1c3ad2d304b11acd1012
BuldakovIvan <iv081198@yandex.ru>
2019-08-14 18:03:51 +0300

    update

---
+++ src/js/global.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let menuToggler = document.querySelector(".menu-toggler");
+
+let menu = document.querySelector(".menu");
+
+menuToggler.addEventListener("click", () => {
+
+    menu.classList.toggle("menu_active");
+    for (let line of menuToggler.children) {
+        line.classList.toggle("menu-toggler__line_active");
+    }
+
+});
